841812aa7a630d1cfb2d70c75f60f6fa9c721867
Alex <art@zoohilldata.com>
2024-11-16 16:03:25 -0700

    try using same method as start / vinxi for getting params

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,6 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
-  preset: "netlify-legacy"
+  preset: "netlify-legacy",
+  compatibilityDate: "2024-11-16"
 });
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,9 +1,13 @@
 import { getQuery } from "h3"
 
 export default eventHandler((event) => {
-  const { name } = getQuery(event);
+  const url = getRequestURL(event);
+  const search = Object.fromEntries(
+    new URLSearchParams(url.search).entries()
+  )
 
-  console.log({name})
+  console.log({url, search})
 
-  return `Hello, ${name}!`;
+  return `Hello, ${JSON.stringify(search)}!`;
 });
+
